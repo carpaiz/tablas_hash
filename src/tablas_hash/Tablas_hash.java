01d866067011bb59c2243b810f759717297453a5
carpaiz <paiz2790@gmail.com>
2022-04-26 17:43:59 -0600

    primer commit

---
+++ src/tablas_hash/Tablas_hash.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablas_hash;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Desarrollo
+ */
+public class Tablas_hash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_tabla frm = new frm_tabla();
+        frm.show();
+        
+        
+    }
+    
+}
